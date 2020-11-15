a3f001e4a1d932a49fd401744c99db1675017a6f
Alan <jehe_18@alu.uabcs.mx>
2020-11-15 16:55:19 -0700

    Creando el header y slider

--- index.php
+++ index.php
@@ -3,15 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-   
-    
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilosIndex.css?v=0.0.1">  
 </head>
 <body>
     
-    <!-- Principal-->
+    <!--CONTENEDOR DE TODA LA PAGINA -->
+    <div class="contenedor">
 
-    <a href="login.php">Index vacio en espera de crear la sección principal</a>
+        <!--===============================
+            INICIO DEL HEADER DE LA PAGINA
+        ===================================-->
+        <header>
+            <div class="wrapper">
+                <div class="logo">
+                    Anzu
+                </div>
+                <nav>
+                    <a href="#">Inicio</a>
+                    <a href="#">Contacto</a>
+                    <a href="login.php">Ingresar</a>
+                    <a href="registrarse.php">Registrarse</a>
+                </nav>
+            </div>
+        </header>
+        <!--===============================
+            FIN DEL HEADER DE LA PAGINA
+        ===================================-->
+    
+        <!--===============================
+            INICIO DEL SLIDER
+        ===================================-->
+        
+        <div class="slider">
+
+            <ul>
+                
+                <li>
+                    <img src="img/miku.png" alt="">
+                </li>
+                <li>
+                    <img src="img/nino.png" alt="">
+                </li>
+               
+
+            </ul>
+
+        </div>
+     
+    
+    </div>
+
+    
+
+
+
 
 
 </body>
