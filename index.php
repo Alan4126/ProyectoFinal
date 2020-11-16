fc50e66e9dbe74395c73d2ec16f7555c4a11dfc3
Alan <jehe_18@alu.uabcs.mx>
2020-11-15 22:43:12 -0700

    Creando seccion de peliculas y subiendo fotos temporales

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/estilosIndex.css?v=0.0.1">  
+    <link rel="stylesheet" href="css/estilosIndex.css?v=0.0.4">  
 </head>
 <body>
     
@@ -50,6 +50,52 @@
             </ul>
 
         </div>
+
+         <!--===============================
+            FIN DEL SLIDER
+        ===================================-->
+
+        <!--===============================
+            INICIO DE LAS PELICULAS
+        ===================================-->
+
+
+        <div class="box-movies">
+                <div class="movies-img">
+                    <img src="img/pelicula1.jpg" alt="" id="img" title="Kono Subarashii Sekai ni Shukufuku wo!: Kurenai Densetsu">
+                   
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula2.jpg" alt="" id="img" title="Tenki no Ko">
+                   
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula3.jpg" alt="" id="img" title="Seishun Buta Yarou wa Yumemiru Shoujo no Yume wo Minai">
+                    
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula4.jpg" alt="" id="img" title="Youjo Senki Movie">
+                    
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula5.jpg" alt="" id="img" title="Kimi no Suizou wo Tabetai">
+                    
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula6.jpg" alt="" id="img" title="Sword Art Online Movie: Ordinal Scale">
+                  
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula7.jpg" alt="" id="img" title="One Piece Film: Gold">
+                  
+                </div>
+                <div class="movies-img">
+                    <img src="img/pelicula8.jpg" alt="" id="img" title="Re:Zero kara Hajimeru Isekai Seikatsu - Memory Snow">
+                  
+                </div>
+        </div>
+
+
      
     
     </div>
