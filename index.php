a9c6b3231ccbfb1cb9ea86b5aa9cfc5dac3d8977
Alan <jehe_18@alu.uabcs.mx>
2020-11-17 13:33:37 -0700

    Cambiando los enlaces de las peliculas a al efecto hover

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/estilosIndex.css?v=0.0.6">  
+    <link rel="stylesheet" href="css/estilosIndex.css?v=0.0.7">  
 </head>
 <body>
     
@@ -17,7 +17,8 @@
         <header>
             <div class="wrapper">
                 <div class="logo">
-                    Anzu
+                    <a href="index.php">Anzu</a>
+                    
                 </div>
                 <nav>
                     <a href="index.php">Inicio</a>
@@ -63,104 +64,116 @@
 
         <div class="box-movies">
 
-                <a href="pelicula_1.php">
+              
                     <div class="movies-img">
-                        
                         <img src="img/pelicula1.jpg" alt="" id="img" title="Kono Subarashii Sekai ni Shukufuku wo!: Kurenai Densetsu">
-                        
-                        
-                        <div class="capa">
-                            <h5>
-                                Kazuma Satou es un chico de preparatoria otaku y hikikomori 
-                                que no suele salir de casa, pero cuando lo hace, un fatídico (y ridículo)
-                                accidente acaba con su vida. En el otro mundo aparece una diosa ante él y 
-                                le propone comenzar de nuevo su vida en un mundo de magia y espada, pero las 
-                                condiciones son un tanto peculiares, así que acabará comenzando de cero como 
-                                aventurero y acompañado de una diosa. </h5>
-                        </div>
+                        <a href="pelicula_1.php">
+                            <div class="capa">
+                                <h5>
+                                    Kazuma Satou es un chico de preparatoria otaku y hikikomori 
+                                    que no suele salir de casa, pero cuando lo hace, un fatídico (y ridículo)
+                                    accidente acaba con su vida. En el otro mundo aparece una diosa ante él y 
+                                    le propone comenzar de nuevo su vida en un mundo de magia y espada, pero las 
+                                    condiciones son un tanto peculiares, así que acabará comenzando de cero como 
+                                    aventurero y acompañado de una diosa. 
+                                </h5>
+                            </div>
+                        </a>
                         <h3>
                             Kono Subarashii Sekai
                         </h3>
                     </div>
-                </a>
+                
                 
                 <div class="movies-img">
                     <img src="img/pelicula2.jpg" alt="" id="img" title="Tenki no Ko">
-                    <div class="capa">
-                        <h5>
-                            La historia se en centra en el estudiante de preparatoria Hodaka, 
-                            quien abandona su hogar en una isla aislada y se muda a Tokio, 
-                            pero inmediatamente pierde todo su dinero. Ahora, viviendo sus días 
-                            en aislamiento, finalmente encuentra un trabajo como escritor para una 
-                            revista.
-                        </h5>
-                    </div>
+                        <a href="pelicula_1.php">
+                            <div class="capa">
+                                <h5>
+                                    La historia se en centra en el estudiante de preparatoria Hodaka, 
+                                    quien abandona su hogar en una isla aislada y se muda a Tokio, 
+                                    pero inmediatamente pierde todo su dinero. Ahora, viviendo sus días 
+                                    en aislamiento, finalmente encuentra un trabajo como escritor para una 
+                                    revista.
+                                </h5>
+                            </div>
+                        </a>
                     <h3>
                         Tenki no Ko
                     </h3>
                 </div>
                 <div class="movies-img">
                     <img src="img/pelicula3.jpg" alt="" id="img" title="Seishun Buta Yarou wa Yumemiru Shoujo no Yume wo Minai">
-                    <div class="capa">
-                        <h5>
-                            En Fujisawa, donde los cielos son brillantes y los mares brillan, 
-                            Sakuta Azusagawa está en su segundo año de secundaria. Sus días felices 
-                            con su novia y estudiante de último año, Mai Sakurajima, se ven interrumpidos 
-                            con la aparición de su primer enamoramiento, Shouko Makinohara. Por razones 
-                            desconocidas, se encuentra con dos Shouko: uno en la escuela secundaria y 
-                            otro que se ha convertido en un adulto.
-                        </h5>
-                    </div>
+                    <a href="pelicula_1.php">
+                        <div class="capa">
+                            <h5>
+                                En Fujisawa, donde los cielos son brillantes y los mares brillan, 
+                                Sakuta Azusagawa está en su segundo año de secundaria. Sus días felices 
+                                con su novia y estudiante de último año, Mai Sakurajima, se ven interrumpidos 
+                                con la aparición de su primer enamoramiento, Shouko Makinohara. Por razones 
+                                desconocidas, se encuentra con dos Shouko: uno en la escuela secundaria y 
+                                otro que se ha convertido en un adulto.
+                            </h5>
+                        </div>
+                    </a>
                     <h3>
                         Seishun Buta Yarou
                     </h3>
                 </div>
                 <div class="movies-img">
                     <img src="img/pelicula4.jpg" alt="" id="img" title="Youjo Senki Movie">
-                    <div class="capa">
-                        <h5>
-                            Año 1926. El Batallón Aéreo de Magos 203 del Ejército Imperial, comandado por 
-                            la Mayor Tanya von Degurechaff, se alzó con la victoria en la batalla del sur 
-                            contra la República. Habiendo regresado bañados en gloria y con las mieles del 
-                            éxito en la boca, esperaban poder descansar un poco, pero el cuartel general les
-                            envía nuevas órdenes especiales en el mismo momento en el que ponen un pie en casa.
-                        </h5>
-                    </div>
+                    <a href="pelicula_1.php">
+                        <div class="capa">
+                            <h5>
+                                Año 1926. El Batallón Aéreo de Magos 203 del Ejército Imperial, comandado por 
+                                la Mayor Tanya von Degurechaff, se alzó con la victoria en la batalla del sur 
+                                contra la República. Habiendo regresado bañados en gloria y con las mieles del 
+                                éxito en la boca, esperaban poder descansar un poco, pero el cuartel general les
+                                envía nuevas órdenes especiales en el mismo momento en el que ponen un pie en casa.
+                            </h5>
+                        </div>
+                    </a>
+                    
                     <h3>
                         Youjo Senki Movie
                     </h3>
                 </div>
                 <div class="movies-img">
                     <img src="img/pelicula5.jpg" alt="" id="img" title="Kimi no Suizou wo Tabetai">
-                    <div class="capa">
-                        <h5>
-                            Un protagonista anónimo encuentra un día un diario en un hospital que pertenece a su compañera 
-                            de clase Sakura Yamauchi. En él descubre que la chica padece una enfermedad terminal en el 
-                            páncreas y que le quedan pocos meses de vida.
-                        </h5>
-                    </div>
+                    <a href="pelicula_1.php">
+                        <div class="capa">
+                            <h5>
+                                Un protagonista anónimo encuentra un día un diario en un hospital que pertenece a su compañera 
+                                de clase Sakura Yamauchi. En él descubre que la chica padece una enfermedad terminal en el 
+                                páncreas y que le quedan pocos meses de vida.
+                            </h5>
+                        </div>
+                    </a>
                     <h3>
                         Kimi no Suizou wo Tabetai
                     </h3>
                 </div>
                 <div class="movies-img">
                     <img src="img/pelicula6.jpg" alt="" id="img" title="Sword Art Online Movie: Ordinal Scale">
-                    <div class="capa">
-                        <h5>
-                            La película tiene lugar tras los hechos acontecidos en la segunda temporada animada.
-                            Kirito, Asuna y sus amigos empiezan a jugar a un nuevo juego conocido como OS (Ordinal Scale) 
-                            que, a diferencia de los juegos anteriores, utiliza un sistema de realidad aumentada llamado 
-                            Augma. En este juego, los jugadores se clasifican de acuerdo a sus rangos por lo que los 
-                            jugadores con rangos inferiores tienen grandes desventajas.
-                        </h5>
-                    </div>
+                    <a href="pelicula_1.php">
+                        <div class="capa">
+                            <h5>
+                                La película tiene lugar tras los hechos acontecidos en la segunda temporada animada.
+                                Kirito, Asuna y sus amigos empiezan a jugar a un nuevo juego conocido como OS (Ordinal Scale) 
+                                que, a diferencia de los juegos anteriores, utiliza un sistema de realidad aumentada llamado 
+                                Augma. En este juego, los jugadores se clasifican de acuerdo a sus rangos por lo que los 
+                                jugadores con rangos inferiores tienen grandes desventajas.
+                            </h5>
+                        </div>
+                    </a>
                     <h3>
                         Sword Art Online Movie
                     </h3>
                 </div>
                 <div class="movies-img">
                     <img src="img/pelicula7.jpg" alt="" id="img" title="One Piece Film: Gold">
-                    <div class="capa">
+                    <a href="pelicula_1.php">
+                        <div class="capa">
                         <h5>
                             La película narra la aventura de los Mugiwara en Gran Tesoro,
                             la ciudad de entretenimiento más grande del mundo y que está bajo el 
@@ -170,21 +183,25 @@
                             por alto por el Gobierno Mundial, quienes reconocen su ciudad como independiente.
                         </h5>
                     </div>
+                    </a>
                     <h3>
                         One Piece Film: Gold
                     </h3>
                 </div>
                 <div class="movies-img">
                     <img src="img/pelicula8.jpg" alt="" id="img" title="Re:Zero kara Hajimeru Isekai Seikatsu - Memory Snow">
-                    <div class="capa">
-                        <h5>
-                            La historia del OVA tiene lugar después de que Subaru y sus amigos derrotan a los Wolgarm,
-                            la fuente de la maldición, y salvan a los niños de la aldea de Irlam. 
-                            Los personajes finalmente obtienen un momento de paz, y Subaru realiza cierta misión 
-                            secreta que no debe dejar que nadie descubra. Sin embargo, a pesar de que Subaru 
-                            lleva un disfraz, Petra y otros niños del pueblo inmediatamente descubren quién es.
-                        </h5>
-                    </div>
+                    <a href="pelicula_1.php">
+                        <div class="capa">
+                            <h5>
+                                La historia del OVA tiene lugar después de que Subaru y sus amigos derrotan a los Wolgarm,
+                                la fuente de la maldición, y salvan a los niños de la aldea de Irlam. 
+                                Los personajes finalmente obtienen un momento de paz, y Subaru realiza cierta misión 
+                                secreta que no debe dejar que nadie descubra. Sin embargo, a pesar de que Subaru 
+                                lleva un disfraz, Petra y otros niños del pueblo inmediatamente descubren quién es.
+                            </h5>
+                        </div>
+                    </a>
+                   
                     <h3>
                          Re:Zero kara Hajimeru
                     </h3>
--- pelicula_1.php
+++ pelicula_1.php
@@ -16,7 +16,7 @@
         <header>
             <div class="wrapper">
                 <div class="logo">
-                    Anzu
+                    <a href="index.php">Anzu</a>
                 </div>
                 <nav class="nav">
                     <a href="index.php">Inicio</a>
